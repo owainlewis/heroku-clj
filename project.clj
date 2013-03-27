640eee3fce88e99876bc67b0f54a154cd106f890
owain lewis <owain@owainlewis.com>
2013-03-27 13:51:36 +0000

    first

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject heroku-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [cheshire "5.0.2"]
+                 [clj-http "0.6.5"]])
---
+++ src/heroku_clj/core.clj
@@ -0,0 +1,62 @@
+(ns heroku-clj.core
+  (:require [clj-http.client :as http]
+            [cheshire.core :as json]))
+
+(def ^:dynamic key
+  (atom ""))
+
+(defn set-api-key! [value]
+  (reset! key value))
+
+(defn symbolize-keys
+  "Take all string keys and turn them into symbols"
+  [m]
+  (into {}
+    (for [[k v] m]
+      [(keyword k) v])))
+
+(defn request
+  "Make a request to the Heroku API
+   - url the request url
+   - api-key your api key
+  "
+  [url api-key]
+  (let [auth-params {:basic-auth ["" api-key]}]
+    (when-let [response (http/get url (merge {:accept :json} auth-params))]
+      (let [result (->> (:body response) json/parse-string)]
+        (if (= (class result) clojure.lang.PersistentHashMap)
+          (symbolize-keys result)
+          (map symbolize-keys result))))))
+
+(defn full-url [path]
+  (str "https://api.heroku.com/" path))
+
+(defn do-request
+  "Performs a request and handles any exceptions"
+  [resource key]
+  (let [u (full-url resource)]
+    (try
+      (request u key)
+    (catch Exception e
+      (print (.getMessage e))))))
+
+;; Apps
+
+(defn app
+ ([key]
+   (do-request "apps" key))
+ ([key app]
+   (do-request (str "apps/" app) key)))
+
+(defn app-create
+  "Create a new application"
+  ([])
+  ([name])
+  ([name stack]))
+
+;; Processes
+
+(defn list-processes
+  "List processes for an app"
+  [key app]
+  (do-request (format "apps/%s/ps" app) key))
